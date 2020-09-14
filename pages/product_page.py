50eed8637c43d083199039eea3d6ff1acd12cf8b
Igor Kim <choson.igor@gmail.com>
2020-09-14 19:29:27 +0300

    Проверка товара в новом методе

--- pages/product_page.py
+++ pages/product_page.py
@@ -5,10 +5,13 @@ import time
 
 class AddingGoods(BasePage):
     def adding_goods(self):
-        name_goods = self.browser.find_element(*AddToBasket.NAME_GOODS).text
-        price_goods = self.browser.find_element(*AddToBasket.PRICE_GOODS).text
         button_add_to_basket = self.browser.find_element(*AddToBasket.ADD_TO_BASKET)
         button_add_to_basket.click()
         self.solve_quiz_and_get_code()
+
+    def check_of_goods(self):
+        name_goods = self.browser.find_element(*AddToBasket.NAME_GOODS).text
+        price_goods = self.browser.find_element(*AddToBasket.PRICE_GOODS).text
         assert name_goods in self.browser.find_element(*AddToBasket.GOODS_IN_BASKET).text
         assert price_goods in self.browser.find_element(*AddToBasket.GOODS_IN_BASKET).text
+        time.sleep(10)
--- test_product_page.py
+++ test_product_page.py
@@ -2,7 +2,8 @@ from pages.product_page import AddingGoods
 
 
 def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
     page = AddingGoods(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.adding_goods()
+    page.check_of_goods()
